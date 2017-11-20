f767ea55551ce56e0a07bc830f2f01af0865066f
Dung-Yi <dungyichao@gmail.com>
2017-11-20 17:58:36 -0500

    Neural Network Architecture

--- DoubleDQN.py
+++ DoubleDQN.py
@@ -9,13 +9,19 @@ class Qnetwork():
 
 
     def structure(self):
+        init = tf.glorot_normal_initializer()
         self.input = tf.placeholder(dtype=tf.float32, shape=[None,self.s_size])
-        self.W_1 = tf.get_variable(shape=[self.s_size,6], dtype=tf.float32, name='w1')
-        self.b_1 = tf.get_variable(shape=[6], dtype=tf.float32, name='b1')
-        h_1 = tf.nn.relu(tf.matmul(self.input, self.W_1) + self.b_1)
+        self.inputt = tf.truediv(self.input,[[180.0,180.0]])
+        self.W_1 = tf.get_variable(shape=[self.s_size,10], dtype=tf.float32, name='w1', initializer=init)
+        self.b_1 = tf.get_variable(shape=[10], dtype=tf.float32, name='b1')
+        h_1 = tf.nn.relu(tf.matmul(self.inputt, self.W_1) + self.b_1)
         self.h_1_drop = tf.nn.dropout(h_1, keep_prob=0.75)
-        self.W_2 = tf.get_variable(shape=[6, self.a_size], dtype=tf.float32, name='w2')
-        self.action = tf.matmul(self.h_1_drop, self.W_2)   # Q value for each action
+        self.W_2 = tf.get_variable(shape=[10,6], dtype=tf.float32, name='w2', initializer=init)
+        self.b_2 = tf.get_variable(shape=[6], dtype=tf.float32, name='b2')
+        h_2 = tf.nn.relu(tf.matmul(h_1, self.W_2) + self.b_2)
+        self.h_2_drop = tf.nn.dropout(h_2, keep_prob=0.75)
+        self.W_3 = tf.get_variable(shape=[6, self.a_size], dtype=tf.float32, name='w3', initializer=init)
+        self.action = tf.matmul(self.h_2_drop, self.W_3)   # Q value for each action
         self.predict = tf.argmax(input=self.action, axis=1)
         self.target_y = tf.placeholder(dtype=tf.float32, shape=[None])
         self.a = tf.placeholder(dtype=tf.int32, shape=[None])
